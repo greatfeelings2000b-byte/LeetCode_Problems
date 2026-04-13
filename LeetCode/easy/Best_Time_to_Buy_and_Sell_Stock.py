f3131c6a828e99203ffa3316536612b87d1e7352
Muhammad Huzaifa Khan <greatfeelings2000b@gmail.com>
2026-04-13 13:28:32 -0700

    Added Best Time to Buy and Sell Stock

---
+++ LeetCode/easy/Best_Time_to_Buy_and_Sell_Stock.py
@@ -0,0 +1,19 @@
+# First I used the brute force in which we uses nested for loop resulting in higher time complexity
+# For optimised solution we used only one loop and used if statements to meet our conditions
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+class Solution:
+    def maxProfit(self, prices):
+        min_price = float('inf')
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            else:
+                profit = price - min_price
+                if profit > max_profit:
+                    max_profit = profit
+
+        return max_profit
